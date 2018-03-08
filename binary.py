4eca88253f1087cbf35c57a578826dc26261fd1d
idgd <isaac.dudney@icloud.com>
2018-03-08 11:32:11 -0700

    made bsearch final draft, moving test code to separate file

--- binary.py
+++ binary.py
@@ -1,5 +1,3 @@
-from generate import gen_list
-
 ### utility func
 
 # finds the middle item
@@ -9,28 +7,28 @@ def half(l):
 ### binary search
 
 def bsearch(l,i):
-	print(l)
+	# what's found
 	found = None
+	# halfway point, start, end
 	h = half(l)
 	s = 0
 	e = len(l) - 1
 	while found != i:
+		# found is the middle item
 		found = l[int(h)]
+		# print where it's at
 		print("half: " + str(int(h)) + ", Searching for: " + str(i))
-		print(e - s, h)
+		# if it's found
 		if i == found:
 			return(True)
+		# if it's not (searchspace is size = 1 and it's not in there)
 		elif e - s == 1:
 			return(False)
+		# if search item is less than the middle, go down
 		elif i < found:
 			e = h
 			h = (s + e)//2
+		# if search item is more than the middle, go up
 		elif i > found:
 			s = h
 			h = (s + e)//2
-
-# generate 1000 item list
-l = gen_list(10)
-# what to find
-i = 5
-print(bsearch(l,i))
