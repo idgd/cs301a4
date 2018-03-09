db8debdd60eead3388358151ebdf5bd4aa33c071
idgd <isaac.dudney@icloud.com>
2018-03-08 23:58:58 -0700

    fixed counting sort

--- sort.py
+++ sort.py
@@ -1,11 +1,30 @@
+from random import shuffle
+from generate import gen_list_unsorted as gen_list
+from time import time
+
+# silly bogosort (shuffles then checks against an already sorted list)
+# this is just for fun :)
+def sort_list_bogo(ulist):
+	s = ulist[:]
+	s.sort()
+	shuffle(ulist)
+	while ulist != s:
+		print(ulist)
+		shuffle(ulist)
+	return(ulist)
+
 # counting sort
-# linear running time
-# non-big-O: 2n + k, where k is the number of unique entries in the list
-def sort_list(ulist):
-	counted = dict((i,0) for i in ulist)
+# faster than python's built-in sort (timsort), but much more memory
+def sort_list_counting(ulist):
+	g = max(ulist)
+	l = min(ulist)
+	c = [0 for f in range(l,g + 1)]
 	for f in ulist:
-		counted[f] += 1
+		c[f] += 1
 	r = []
-	for f in counted:
-		r += [f] * counted[f]
+	i = 0
+	while i < len(c):
+		nl = [i] * c[i]
+		r.extend(nl)
+		i += 1
 	return(r)
