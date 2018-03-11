7a1a73c79554ad57a704216059eaa6baeaae26c3
idgd <isaac.dudney@icloud.com>
2018-03-11 10:31:56 -0600

    added search test code (bottom half, not in list, top half)

--- test.py
+++ test.py
@@ -3,9 +3,10 @@ import generate
 import hashtable
 import sort
 
-sorted_list = generate.gen_list(1000)
-unsorted_list = generate.gen_list_unsorted(1000)
+sorted_list = generate.gen_list(1024)
+unsorted_list = generate.gen_list_unsorted(1024)
 
+print("Hashtable test")
 hashtable = hashtable.ht(5)
 hashtable.put(5)
 hashtable.put(5)
@@ -18,3 +19,12 @@ hashtable.put(1)
 print(hashtable.contains(5))
 print(hashtable.contains(50))
 print(hashtable.items())
+
+print("\nBinary Search test")
+print(binary.bsearch(sorted_list,256))
+print("")
+print(binary.bsearch(sorted_list,4096))
+print("")
+print(binary.bsearch(sorted_list,768))
+
+print("\nSorting test")
