2b30958ce4be9d4afc888886c612cf1b56d09e7d
idgd <isaac.dudney@icloud.com>
2018-03-18 07:32:19 -0700

    fixed creation of mostly sorted lists in generate

--- generate.py
+++ generate.py
@@ -4,12 +4,12 @@ from random import randint
 def gen_list(n):
 	r = []
 	for f in range(n):
-		r.append(randint(0,f))
+		r.append(randint(0,n))
 	r.sort()
 	return(r)
 
 def gen_list_unsorted(n):
 	r = []
 	for f in range(n):
-		r.append(randint(0,f))
+		r.append(randint(0,n))
 	return(r)
