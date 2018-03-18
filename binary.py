4167d3faad84229f6a5baab181bfec8bd6a659ff
idgd <isaac.dudney@icloud.com>
2018-03-17 22:31:24 -0700

    fixed binary, made division floating point instead of integer; more elegant fix probable, but I am tired

--- binary.py
+++ binary.py
@@ -1,38 +1,24 @@
-### utility func
-
-# finds the middle item
-def half(l):
-	return(l[int(len(l)/2)])
-
-### binary search
-
 def bsearch(l,i):
-	# testing found edgecase: won't find first/last items
-	if l[0] == i or l[-1] == i:
-		return(True)
-
 	# what's found
 	found = None
 	# halfway point, start, end
-	h = half(l)
+	h = l[int(len(l)/2)]
 	s = 0
 	e = len(l) - 1
 	while found != i:
 		# found is the middle item
 		found = l[int(h)]
-		# print where it's at
-		print("half: " + str(int(h)) + ", Searching for: " + str(i))
 		# if it's found
 		if i == found:
 			return(True)
 		# if it's not (searchspace is size = 1 and it's not in there)
-		elif e - s == 1:
+		elif e - s == 0:
 			return(False)
 		# if search item is less than the middle, go down
 		elif i < found:
 			e = h
-			h = (s + e)//2
+			h = (s + e)/2
 		# if search item is more than the middle, go up
 		elif i > found:
 			s = h
-			h = (s + e)//2
+			h = (s + e)/2
